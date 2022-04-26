3837adf64f6af63b9ec9334770ce79d0c86557ed
car0stef <stefani.aishwarya@gmail.com>
2022-04-26 16:36:48 -0300

    primer desafio

--- main.js
+++ main.js
@@ -1,66 +1,92 @@
-for(let i=1; i<=3 ; i++){
-    let nombre = prompt("Ingrese nombre");
-    let number = parseInt(prompt("Ingrese edad"));
+function totalNinio(cuanto , remera){
+    cuanto = parseInt(prompt("¿Cuántas remeras desea comprar?"));
+    remera = 1500;
+    let resultado = cuanto * remera;
+    return alert(`El total de su compra es: ${resultado} $`);
+}
+function totalAdulto(cuanto , remera){
+    cuanto = parseInt(prompt("¿Cuántas remeras desea comprar?"));
+    remera = 2300;
+    let resultado = cuanto * remera;
+    return alert(`El total de su compra es: ${resultado} $`);
+}
+function saludo(){
+    alert("Gracias por su compra. Vuelva Pronto");
+}
 
-if(number <= 12){
-    alert (`${i} ${nombre}, su turno pasa al día lunes`);
-    let nombre2 = prompt("¿Desea ingresar otro nombre? si/no");
-        if((nombre2 == "no")||(nombre2 == "NO")){
-            alert("Hasta luego, gracias.");
-            break;
-        }else if((nombre2 == "si")|| (nombre2 == "SI")){
-            alert("Continuamos...");
-            while (i === 3) {
-                alert("Se completaron 3 veces los campos, necesitas comenzar de nuevo");
-                break;
+function talle(){
+    let tipoPersona = prompt("Comprar remeras para: Niño (1) Adulto (2)");    
+    
+        if (tipoPersona == "1") {
+            let ninio = prompt("¿Qué talle desea seleccionar? 4 , 6 , 8 , 10");
+            switch (ninio) {
+                case "4":
+                    alert(`Se agregó correctamente el talle ${ninio}`);
+                    totalNinio();
+                    saludo();                        
+                    break;
+                    
+                case "6":
+                    alert(`Se agregó correctamente el talle ${ninio}`);
+                    totalNinio();
+                    saludo();                        
+                    break;
+
+                case "8":
+                    alert(`Se agregó correctamente el talle ${ninio}`);
+                    totalNinio();
+                    saludo();                        
+                    break;
+
+                case "10":
+                    alert(`Se agregó correctamente el talle ${ninio}`);
+                    totalNinio();
+                    saludo();                        
+                    break;
+            
+                default:
+                    alert("Ud. necesita ingresar un talle válido");                        
+                    break;
             }
-        }else{
-            alert("Ud. necesita afirmar o denegar");
         }
-}else if( number <= 20){
-    alert (`${i} ${nombre}, su turno pasa al día martes`);
-    let nombre2 = prompt("¿Desea ingresar otro nombre? si/no");
-        if((nombre2 == "no")||(nombre2 == "NO")){
-            alert("Hasta luego, gracias.");
-            break;
-        }else if((nombre2 == "si")|| (nombre2 == "SI")){
-            alert("Continuamos...");
-            while (i === 3) {
-                alert("Se completaron 3 veces los campos, necesitas comenzar de nuevo");
-                break;
-            }
-        }else{
-            alert("Ud. necesita afirmar o denegar");
+        else if (tipoPersona == "2"){
+            let adulto = prompt("¿Qué talle desea seleccionar? S, M, L, XL");
+            
+                switch (adulto) {            
+                    case "s":
+                        alert(`Se agregó correctamente el talle ${adulto}`);
+                        totalAdulto();
+                        saludo();
+                        break;
+                        
+                    case "m":
+                        alert(`Se agregó correctamente el talle ${adulto}`);
+                        totalAdulto();
+                        saludo();
+                        break;
+
+                    case "l":
+                        alert(`Se agregó correctamente el talle ${adulto}`);
+                        totalAdulto();
+                        saludo();
+                        break;
+
+                    case "xl":
+                        alert(`Se agregó correctamente el talle ${adulto}`);
+                        totalAdulto();
+                        saludo();
+                        break;
+                
+                    default:
+                        alert("Ud. necesita ingresar una talla válida")
+                        break;
+                }
         }
-}else if( number <= 40){
-    alert (`${i} ${nombre}, su turno pasa al día miercoles`);
-    let nombre2 = prompt("¿Desea ingresar otro nombre? si/no");
-        if((nombre2 == "no")||(nombre2 == "NO")){
-            alert("Hasta luego, gracias.");
-            break;
-        }else if((nombre2 == "si")|| (nombre2 == "SI")){
-            alert("Continuamos...");
-            while (i === 3) {
-                alert("Se completaron 3 veces los campos, necesitas comenzar de nuevo");
-                break;
-            }
-        }else{
-            alert("Ud. necesita afirmar o denegar");
+        else{
+            alert("Debes ingresar un talle existente");
         }
-}else{
-    alert (`${nombre}, Ud. pasa sin turno`);
-    let nombre2 = prompt("¿Desea ingresar otro nombre? si/no");
-        if((nombre2 == "no")||(nombre2 == "NO")){
-            alert("Hasta luego, gracias.");
-            break;
-        }else if((nombre2 == "si")|| (nombre2 == "SI")){
-            alert("Continuamos...");
-            while (i===3){
-                alert("Se completaron 3 veces los campos, necesitas comenzar de nuevo");
-                break;
-            }
-        }else{
-            alert("Ud. necesita afirmar o denegar");
-        }
-    }
+}
+
+for (let i = 0; i <=2 ; i++){
+    talle();
 }
